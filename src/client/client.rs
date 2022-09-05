2755a0400a82b30feb97e1b894520a54124b0745
Daniil Rybalko <pslpslpslpsl@yandex.ru>
2022-09-05 23:39:16 +0300

    upd client

--- src/client/client.rs
+++ src/client/client.rs
@@ -1,20 +1,61 @@
 use notify_rust::Notification;
 use utils;
+use serde_json;
+use serde;
 use std::{
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
 
-struct Client{
+#[derive(serde::Deserialize, Debug, Clone)]
+pub struct Client{
     name : String,
     adrs : String,
+    port : String
 }
 
 impl Client{
-    pub fn new(name : &String, adrs : &String) -> Self{
+    pub fn init_from_json(file: String) -> Self{
+        let data = std::fs::read_to_string(&file).expect("Unable to read file");
+        let parsed_client: Client = serde_json::from_str(&data).unwrap();
+        return parsed_client;
+    }
+
+    pub fn new(name : &String, adrs : &String, port : &String) -> Self{
         return Client{ 
             name : name.to_string(), 
-            adrs : adrs.to_string() };
+            adrs : adrs.to_string(),
+            port : port.to_string() };
+    }
+
+    pub fn scan_network(self){
+        let ips: Vec<i32> = (0..255).map(|v| v).collect();
+        let parsed_net: Vec<&str> = self.adrs.split(".").collect();
+        let mut total_conns = 0;
+        for ip in ips{
+            println!("{}", &ip);
+            let curr_adr = parsed_net[0].to_string()
+            + "." + &parsed_net[1].to_string()
+            + "." + &parsed_net[2].to_string()
+            + "." + &ip.to_string() + ":" + &self.port;
+            let curr_connect_result = std::net::TcpStream::connect(&curr_adr);
+            if curr_connect_result.is_ok() {
+                println!("Connection established at {}", &curr_adr);
+                total_conns += 1;
+            }
+        }
+        println!("Total {} devices discovered.", &total_conns.to_string());
+    }
+
+    pub fn to_string(self) -> String{
+        let mut return_str = String::new();
+        return_str.push_str("Client ");
+        return_str.push_str(&self.name);
+        return_str.push_str(", network ");
+        return_str.push_str(&self.adrs);
+        return_str.push_str(", port ");
+        return_str.push_str(&self.port);
+        return return_str;
     }
 }
 
@@ -23,3 +64,4 @@ pub fn send_request(body : &String){
     stream.write(body.as_bytes());
     stream.flush();
 }
+
--- src/client/main.rs
+++ src/client/main.rs
@@ -2,6 +2,10 @@ mod client;
 
 fn main(){
     println!("Client started... \n Form message as %message|code%");
+    let clnt = client::Client::init_from_json(("client_config.json").to_string());
+    let clnt2 = clnt.clone();
+    clnt.scan_network();
+    println!("{}", &clnt2.to_string());
     loop{
         let mut msg = String::new();
         std::io::stdin()
